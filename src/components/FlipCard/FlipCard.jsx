85b41a55820a9f187e435d0d8ee4066c224b089c
tony-sn <tony.sn@outlook.com>
2021-12-31 03:03:11 +1100

    Added flipcard back content

--- src/components/FlipCard/FlipCard.jsx
+++ src/components/FlipCard/FlipCard.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './FlipCard.scss'
 import Theme from "../../assets/img/FairyTheme.png"
+import Location from "../../assets/img/ChipmunksLocation.jpg"
 
 function FlipCard() {
   const [showBack, setShowBack] = useState(false)
@@ -19,7 +20,7 @@ function FlipCard() {
             <p className="card-text text-sm md:text-base">It would be very nice of you to come to</p>
             <p className="card-text text-sm md:text-base font-bold">Alana's Birthday Party</p>
             <p className="card-text text-sm md:text-base">Date: <span>02/01/22</span> &nbsp; Time: <span>11:45 AM</span></p>
-            <p className="card-text text-sm md:text-base">Place: <span>Chipmunks Playland</span></p>
+            <p className="card-text text-sm md:text-base">Place: <span>Chipmunks Playland & Cafe</span></p>
             <div className="card-text text-center text-sm italic">
               <p>Don't forget to bring your socks</p>
               <p>Valid for 1 child only</p>
@@ -30,8 +31,19 @@ function FlipCard() {
           </div>
         </div>
         <div className="card back">
+        <img className="card-img-top" src={Location} alt="Chipmunks Location"/>
           <div className="card-body">
-            <p className="card-text text-lg font-bold">Back</p>
+          <h5 className="card-title sm:text-sm font-semibold">Address: 3/1099 Western Hwy, Ravenhall VIC 3023</h5>
+            <p className="card-text text-sm md:text-base">(Slip lane from Ballarat Rd, next to Total Tools and Fantastic Furniture)</p>
+            <p className="card-text text-sm md:text-base font-bold">Menu</p>
+            <div className="card-text text-sm italic">
+              <p>Birthday Cake</p>
+              <p>Chicken Nuggets, Spring Rolls, Chips, etc.</p>
+              <p>03 Pizzas (included Vegetarian one)</p>
+              <p>Fruit Platter</p>
+              <br/>
+              <p>*No outside food allowed</p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     <div className="home">
       <h1 className="text-xl text-white font-bold text-center">💖 Happy birthday, <span style={{color: "rgb(255, 22, 209)"}}>Alana</span> ! 🎉</h1>
       <FlipCard />
+      <h2 className="text-lg text-white font-bold text-center">Can't wait to see you there.</h2>
     </div>
   )
 }
